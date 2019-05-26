34e3c3eac075448f64332ab32f30d523ab6eacc1
João Gonçalves <miguel.joao.goncalves@gmail.com>
2019-05-26 15:00:39 +0200

    add i2c

---
+++ inc/i2c/hal_i2c.c
@@ -0,0 +1,103 @@
+
+/* This function issues a start condition and
+ * transmits the slave address + R/W bit
+ *
+ * Parameters:
+ * I2Cx --> the I2C peripheral e.g. I2C1
+ * address --> the 7 bit slave address
+ * direction --> the transmission direction can be:
+ * I2C_Direction_Tranmitter for Master transmitter mode
+ * I2C_Direction_Receiver for Master receiver
+ */
+#include "hal_i2c.h"
+
+void I2C_start(I2C_TypeDef* I2Cx, uint8_t address, uint8_t direction)
+{
+	// wait until I2C1 is not busy any more
+
+	//UB_Uart_SendString(COM2, "I2C Start wait on Busy Flag...", LFCR);
+	while(I2C_GetFlagStatus(I2Cx, I2C_FLAG_BUSY));
+	//UB_Uart_SendString(COM2, "I2C Generate start", LFCR);
+	// Send I2C1 START condition : SDA =0, SCL=0
+	I2C_GenerateSTART(I2Cx, ENABLE);
+	//UB_Uart_SendString(COM2, "I2C wait acknowlege from slave", LFCR);
+	// wait for I2C1 EV5 --> Slave has acknowledged start condition
+	while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_MODE_SELECT));
+	//UB_Uart_SendString(COM2, "I2C Slave address for write", LFCR);
+	// Send slave Address for write
+	I2C_Send7bitAddress(I2Cx, address, direction);
+
+	/* wait for I2Cx EV6, check if
+	 * either Slave has acknowledged Master transmitter or
+	 * Master receiver mode, depending on the transmission
+	 * direction
+	 */
+	if(direction == I2C_Direction_Transmitter)
+	{//UB_Uart_SendString(COM2, "I2C wait on transmitter mode", LFCR);
+		while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+	}
+	else if(direction == I2C_Direction_Receiver)
+	{//UB_Uart_SendString(COM2, "I2C wait on receiver mode", LFCR);
+		while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
+	}
+
+}
+
+/* This function transmits one byte to the slave device
+ * Parameters:
+ * I2Cx --> the I2C peripheral e.g. I2C1
+ * data --> the data byte to be transmitted
+ */
+void I2C_write(I2C_TypeDef* I2Cx, uint8_t data)
+{
+	// wait for I2C1 EV8 --> last byte is still being transmitted (last byte in SR, buffer empty), next byte can already be written
+	while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_TRANSMITTING));
+	I2C_SendData(I2Cx, data);
+}
+
+/* This function reads one byte from the slave device
+ * and acknowledges the byte (requests another byte)
+ */
+uint8_t I2C_read_ack(I2C_TypeDef* I2Cx)
+{
+	uint8_t data;
+	// enable acknowledge of received data
+	I2C_AcknowledgeConfig(I2Cx, ENABLE);
+	// wait until one byte has been received : EV7
+	while( !I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_RECEIVED) );
+	// read data from I2C data register and return data byte
+	data = I2C_ReceiveData(I2Cx);
+	return data;
+}
+
+/* This function reads one byte from the slave device
+ * and doesn't acknowledge the received data
+ * after that a STOP condition is transmitted
+ */
+uint8_t I2C_read_nack(I2C_TypeDef* I2Cx)
+{
+	uint8_t data;
+	// disable acknowledge of received data
+	// nack also generates stop condition after last byte received
+	// see reference manual for more info
+	I2C_AcknowledgeConfig(I2Cx, DISABLE);
+	I2C_GenerateSTOP(I2Cx, ENABLE);
+	// wait until one byte has been received : EV7
+	while( !I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_RECEIVED) );
+	// read data from I2C data register and return data byte
+	data = I2C_ReceiveData(I2Cx);
+	return data;
+}
+
+/* This function issues a stop condition and therefore
+ * releases the bus
+ */
+void I2C_stop(I2C_TypeDef* I2Cx)
+{
+
+	// Send I2C1 STOP Condition after last byte has been transmitted
+	I2C_GenerateSTOP(I2Cx, ENABLE);
+	// wait for I2C1 EV8_2 --> byte has been transmitted
+	while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+}
+
